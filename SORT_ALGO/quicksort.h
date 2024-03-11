b18e82a89615fe986a719c0ed5fca80a096c3f63
Kur1boh <81129015+hxzinh@users.noreply.github.com>
2024-03-11 21:53:38 +0700

    Update quicksort.h

--- SORT_ALGO/quicksort.h
+++ SORT_ALGO/quicksort.h
@@ -1,25 +1,44 @@
 class QuickSort {
 public:
-    static int Partition(double arr[], int l, int r, double pivot){
-        int id = l;
+    static void swap(double *a, double *b) {
+        double c = *a;
+        *a = *b;
+        *b = c;
+    }
 
-        for(int i = l; i <= r; i++) {
-            if(arr[i] <= pivot) {
-                double cur = arr[i];
-                arr[i] = arr[id];
-                arr[id] = cur;
-                id++;
-            }
+    static int partition_pivot_last(double array[], int low, int high) {
+        double pivot = array[high];
+        int i = (low - 1);
+ 
+        for(int j = low; j < high; j++) {
+            if (array[j] < pivot)
+                swap(&array[++i], &array[j]);
         }
+ 
+        swap(&array[i + 1], &array[high]);
+        return (i + 1);
+    }
 
-        id--;
-        return id;
+    static int Partition(double array[], int low, int high) {
+        double pivot;
+        int mid = (low + high) / 2;
+        if (array[mid] < array[low]) 
+            swap(&array[mid], &array[low]);
+        if (array[high] < array[low])
+            swap(&array[high], &array[low]);
+        if (array[high] < array[mid])
+            swap(&array[high], &array[mid]);
+
+        swap(&array[mid], &array[high - 1]);
+     
+        pivot = array[high - 1];
+     
+        return partition_pivot_last(array, low, high);
     }
 
     static void quickSort(double arr[], int l, int r){
         if(l < r) {
-            double pivot = arr[r];
-            int id = Partition(arr, l, r, pivot);
+            int id = Partition(arr, l, r);
 
             quickSort(arr, l, id - 1);
             quickSort(arr, id + 1, r);
